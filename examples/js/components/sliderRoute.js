971b37c8d0d093ace433de877cddd04e639cb034
Fahri Soenmez <fahri.soenmez@hbm.com>
2016-09-13 08:56:28 +0200

    add playground to Slider example

--- examples/js/components/sliderRoute.js
+++ examples/js/components/sliderRoute.js
@@ -1,6 +1,12 @@
 
 import React from 'react'
+import Playground from 'component-playground'
 import {Slider} from '../../../'
+
+const defaultSlider = `<input type='range' min='0' max='100' />`
+const defaultSliderWithSteps = `<input type='range' min='0' max='100' step='10' />`
+const slider = `<Slider />`
+const sliderWithSteps = `<Slider step={10} />`
 
 export default class SliderRoute extends React.Component {
 
@@ -9,19 +15,33 @@ export default class SliderRoute extends React.Component {
       <div>
         <section>
           <h2>Default slider</h2>
-          <input type='range' min='0' max='100' />
+          <Playground
+            codeText={defaultSlider}
+            scope={{React}}
+          />
         </section>
         <section>
           <h2>Default slider with steps</h2>
-          <input type='range' min='0' max='100' step='10' />
+          <Playground
+            codeText={defaultSliderWithSteps}
+            scope={{React}}
+          />
         </section>
         <section>
           <h2>Slider</h2>
-          <Slider />
+          <Playground
+            docClass={Slider}
+            codeText={slider}
+            scope={{React, Slider}}
+          />
         </section>
         <section>
           <h2>Slider with steps</h2>
-          <Slider step={10} />
+          <Playground
+            docClass={Slider}
+            codeText={sliderWithSteps}
+            scope={{React, Slider}}
+          />
         </section>
       </div>
     )
