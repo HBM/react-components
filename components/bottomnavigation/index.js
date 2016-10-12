defb9be930f06544fed6872e8e27403e5e12c245
Gerhard Preuss <gelipp@gmail.com>
2016-10-12 14:57:14 +0200

    add inverted prop

--- components/bottomnavigation/index.js
+++ components/bottomnavigation/index.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import {Link} from 'react-router'
+import classNames from 'classnames'
 
 export default class BottomNavigation extends React.Component {
   render () {
-    const {children, links} = this.props
+    const {children, links, inverted} = this.props
     return (
       <div className='BottomNavigation'>
         <div className='BottomNavigation-content'>
           {children}
         </div>
-        <div className='BottomNavigation-menu'>
+        <div className={classNames('BottomNavigation-menu', {'BottomNavigation-menu--inverted': inverted})}>
           {links.map(link => (
             <Link activeClassName='active' to={link.link} className='BottomNavigation-menu-item' key={link.text}>
               {link.icon}
