e7a6c1449453fecddfa4b689f1a45b38db67ffd4
Mirco Zeiss <mirco.zeiss@gmail.com>
2016-10-13 17:05:01 +0200

    start working on stepper component

---
+++ components/stepper/index.js
@@ -0,0 +1,86 @@
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import classnames from 'classnames'
+import {Subscriber} from 'react-broadcast'
+import {Button} from '../../'
+import {Link, Match} from 'react-router'
+
+const Step = ({index, step, isActive, isLast, onContinue, onCancel}) => (
+  <div className='Stepper-step'>
+    <Link
+      className='Stepper-title'
+      activeClassName='is-active'
+      to={step.href}
+    >
+      <div className={classnames('Stepper-circle', {
+        'is-active': isActive
+      })}>
+        {index + 1}
+      </div>
+      {step.title}
+    </Link>
+    <div className={classnames('Stepper-body', {
+      'is-last': isLast
+    })}>
+      <div className='Stepper-line-wrapper'>
+        <div className='Stepper-line' />
+      </div>
+      <div className='Stepper-content-wrapper'>
+        <div className='Stepper-content'>
+          <Match pattern={step.href} component={step.component} />
+        </div>
+        {(!isActive || isLast) ? null : <div className='Stepper-footer'>
+          <Button onClick={onContinue}>
+            Continue
+          </Button>
+          <Button onClick={onCancel}>
+            Cancel
+          </Button>
+        </div>}
+      </div>
+    </div>
+  </div>
+)
+
+export default class Stepper extends React.Component {
+
+  static contextTypes = {
+    router: React.PropTypes.object
+  }
+
+  onContinue = (index) => {
+    const next = this.props.steps[index + 1]
+    if (next) {
+      this.context.router.transitionTo(next.href)
+    }
+  }
+
+  onCancel = (index) => {
+    console.log(`canceled at step ${index + 1}`)
+  }
+
+  render () {
+    return (
+      <Subscriber channel='location'>
+        {location => (
+          <div className='Stepper'>
+            {this.props.steps.map((s, i) => (
+              <Step
+                key={i}
+                index={i}
+                step={s}
+                isActive={location.pathname === s.href}
+                isLast={this.props.steps.length - 1 === i}
+                onContinue={() => this.onContinue(i)}
+                onCancel={() => this.onCancel(i)}
+              />
+            ))}
+          </div>
+        )}
+      </Subscriber>
+    )
+  }
+
+}
+
---
+++ examples/js/components/stepperRoute.js
@@ -0,0 +1,81 @@
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Playground from 'component-playground'
+import {HashRouter} from 'react-router'
+import {Stepper} from '../../../'
+
+const stepperComponent =
+`
+const step1 = () => (
+  <div style={{width: '100%', height: 300, background: '#bdbdbd'}} />
+)
+
+const step2 = () => (
+  <div>step2</div>
+)
+
+const step3 = () => (
+  <div>
+    <p>
+      well done! you've completed all steps and you're the best.
+    </p>
+    <p>
+      <a href='#'>go to main page</a>
+    </p>
+  </div>
+)
+
+class App extends React.Component {
+
+  render () {
+    const steps = [
+      {
+        title: 'Select campaign settings',
+        href: '/stepper/settings',
+        component: step1
+      },
+      {
+        title: 'Create an ad group',
+        href: '/stepper/group',
+        component: step2
+      },
+      {
+        title: 'Create an ad',
+        href: '/stepper/create',
+        component: step3
+      }
+    ]
+    return (
+      <HashRouter>
+        <div>
+          <Stepper steps={steps}/>
+        </div>
+      </HashRouter>
+    )
+  }
+
+}
+
+ReactDOM.render(<App />, mountNode)`
+
+export default class StepperRoute extends React.Component {
+
+  render () {
+    return (
+      <div>
+        <section>
+          <h2>Stepper Component</h2>
+          <Playground
+            docClass={Stepper}
+            codeText={stepperComponent}
+            scope={{React, ReactDOM, Stepper, HashRouter}}
+            noRender={false}
+            collapsableCode
+          />
+        </section>
+      </div>
+    )
+  }
+
+}
