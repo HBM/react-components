67bc32a0ccefad6690abc0df9e2e526467cdfbc9
Fahri Soenmez <fahri.soenmez@hbm.com>
2016-11-01 13:49:15 +0100

    fix stepper test

--- components/stepper/test/test.js
+++ components/stepper/test/test.js
@@ -4,7 +4,8 @@ import assert from 'assert'
 import React from 'react'
 import {mount} from 'enzyme'
 import {Broadcast} from 'react-broadcast'
-import Stepper from '../'
+import {Button} from '../../..'
+import {Stepper, StepperStepFooter} from '../'
 
 describe('Stepper', () => {
   it('should work', () => {
@@ -29,6 +30,104 @@ describe('Stepper', () => {
     assert.equal(wrapper.find('.Stepper').length, 1)
   })
 
+  it('should work horizontal', () => {
+    const location = {pathname: '', search: '', hash: ''}
+    const steps = [
+      {
+        title: 'first',
+        href: '/first',
+        component: () => <div>first</div>
+      },
+      {
+        title: 'second',
+        href: '/second',
+        component: () => <div>second</div>
+      }
+    ]
+    const wrapper = mount(
+      <Broadcast channel='location' value={location}>
+        <Stepper steps={steps} horizontal />
+      </Broadcast>
+    )
+    assert.equal(wrapper.find('.Stepper--horizontal').length, 1)
+  })
+
+  it('should work with optional text', () => {
+    const location = {pathname: '', search: '', hash: ''}
+    const optionalText = 'Optional'
+    const steps = [
+      {
+        title: 'first',
+        href: '/first',
+        optional: optionalText,
+        component: () => <div>first</div>
+      },
+      {
+        title: 'second',
+        href: '/second',
+        component: () => <div>second</div>
+      }
+    ]
+    const wrapper = mount(
+      <Broadcast channel='location' value={location}>
+        <Stepper steps={steps} />
+      </Broadcast>
+    )
+    assert.equal(wrapper.find('.Stepper-step-optional').length, 1)
+    assert(wrapper.find('.Stepper-step-optional').text(), optionalText)
+  })
+
+  it('should work with error', () => {
+    const location = {pathname: '', search: '', hash: ''}
+    const errorText = 'my error'
+    const steps = [
+      {
+        title: 'first',
+        href: '/first',
+        error: {message: errorText},
+        component: () => <div>first</div>
+      },
+      {
+        title: 'second',
+        href: '/second',
+        component: () => <div>second</div>
+      }
+    ]
+    const wrapper = mount(
+      <Broadcast channel='location' value={location}>
+        <Stepper steps={steps} />
+      </Broadcast>
+    )
+    assert.equal(wrapper.find('.Stepper--error').length, 2)
+    assert(wrapper.find('.Stepper--error > .Stepper-step-optional').text(), errorText)
+  })
+
+  it('should work StepperStepFooter', () => {
+    const wrapper = mount(
+      <StepperStepFooter />
+    )
+    assert.equal(wrapper.find('.Stepper-footer').length, 1)
+  })
+
+  it('should work StepperStepFooter with labels and callbacks', () => {
+    const back = (index) => {}
+    const cancel = () => {}
+    const wrapper = mount(
+      <StepperStepFooter
+        labelBack='Back'
+        labelNext='Next'
+        labelCancel='Cancel'
+        onBack={back}
+        onNext={back}
+        onCancel={cancel}
+      />
+    )
+    assert.equal(wrapper.find('.Stepper-footer').length, 1)
+    assert.equal(wrapper.find('.Stepper-step-back').length, 1)
+    assert.equal(wrapper.find('.Stepper-step-next').length, 1)
+    assert.equal(wrapper.find('.Stepper-step-cancel').length, 1)
+  })
+
   it('should apply active class name to currently active step', () => {
     const location = {pathname: '/second', search: '', hash: ''}
     const steps = [
@@ -51,31 +150,28 @@ describe('Stepper', () => {
     assert(wrapper.find('a[href="/second"]').hasClass('is-active'))
   })
 
-  it('should automatically show continue and cancel buttons for all steps except the last one', () => {
-    const location = {pathname: '/first', search: '', hash: ''}
-    const steps = [
-      {
-        title: 'first',
-        href: '/first',
-        component: () => <div>first</div>
-      },
-      {
-        title: 'second',
-        href: '/second',
-        component: () => <div>second</div>
-      }
-    ]
-    const wrapper = mount(
-      <Broadcast channel='location' value={location}>
-        <Stepper steps={steps} />
-      </Broadcast>
-    )
-    assert.equal(wrapper.find('.Stepper-step').at(0).find('Button').length, 2)
-    assert.equal(wrapper.find('.Stepper-step').at(1).find('Button').length, 0)
-  })
-
-  // it('should continue to next step when clicking on continue button', () => {
-  //   const location = {pathname: '/first', search: '', hash: ''}
+  // it('should apply active class name to currently active step and StepperStepFooter test next', () => {
+  //   const location = {pathname: '/second', search: '', hash: ''}
+  //   const step = ({index, isLast, cancel, back, next, error}) => (
+  //     <div>
+  //       <h3>step second</h3>
+  //       <Button onClick={() => error(new Error('My Error'))}>
+  //         Set error
+  //       </Button>
+  //       <Button onClick={() => error()}>
+  //         Clear error
+  //       </Button>
+  //       <StepperStepFooter
+  //         labelBack='Back'
+  //         labelNext='Next'
+  //         labelCancel='Cancel'
+  //         onBack={back}
+  //         onNext={next}
+  //         onCancel={cancel}
+  //       />
+  //     </div>
+  //   )
+  //
   //   const steps = [
   //     {
   //       title: 'first',
@@ -85,19 +181,53 @@ describe('Stepper', () => {
   //     {
   //       title: 'second',
   //       href: '/second',
-  //       component: () => <div>second</div>
+  //       component: step
+  //     },
+  //     {
+  //       title: 'third',
+  //       href: '/third',
+  //       component: () => <div>third</div>
   //     }
   //   ]
+  //
+  //   const onCancel = (index) => {
+  //     // let num = index + 1
+  //     // console.log('canceled at step' + num)
+  //   }
+  //
+  //   const onError = (index, message) => {
+  //     // let steps = this.state.steps.slice()
+  //     // if (index > 0 && index < steps.length) {
+  //     //   steps[index].error = message
+  //     //   this.setState({
+  //     //     steps: steps
+  //     //   })
+  //     // }
+  //   }
+  //
+  //   const transitionTo = (href) => {
+  //     console.log(href)
+  //   }
   //   const wrapper = mount(
   //     <Broadcast channel='location' value={location}>
-  //       <Stepper steps={steps} />
+  //       <Stepper
+  //         steps={steps}
+  //         onError={(i, me) => onError(i, me)}
+  //         onCancel={(i) => onCancel(i)}
+  //       />
   //     </Broadcast>
-  //   , {
-  //     context: {router: HashRouter}
-  //   })
-  //   wrapper.find('.Stepper-step').at(0).find('Button').at(0).simulate('click')
+  //   , {context: {transitionTo: {transitionTo}}})
+  //   assert(wrapper.find('a[href="/second"]').hasClass('is-active'))
+  //   console.log(wrapper.find('.Stepper-step-next').childAt(0).html())
+  //   wrapper.find('.Stepper-step-next').childAt(0).simulate('click')
+  //   // assert(wrapper.find('a[href="/third"]').hasClass('is-active'))
+  //   // wrapper.find('.Stepper-title .Stepper-title-text')[1].simulate('click')
+  //   // assert(wrapper.find('a[href="/second"]').hasClass('is-active'))
+  //   // wrapper.find('.Stepper-step-back .Button').simulate('click')
+  //   // wrapper.find('.Stepper-title .Stepper-title-text')[1].simulate('click')
+  //   // assert(wrapper.find('a[href="/second"]').hasClass('is-active'))
+  //   // wrapper.find('.Stepper-step-cancel .Button').simulate('click')
+  //   console.log('--------------------------------------------------------------')
   //   console.log(wrapper.html())
-  //   // assert.equal(wrapper.find('.Stepper-step').at(0).find('Button').length, 2)
-  //   // assert.equal(wrapper.find('.Stepper-step').at(1).find('Button').length, 0)
   // })
 })
