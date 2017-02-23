70fff98599249b30923bf664647a038259017b87
Fahri Soenmez <fahri.soenmez@hbm.com>
2017-02-23 13:44:45 +0100

    add test

--- src/js/select/test/test.js
+++ src/js/select/test/test.js
@@ -216,4 +216,8 @@ describe('Select', () => {
     wrapper.find('.Select-body').simulate('click')
     assert.equal(wrapper.find('.Select-list').node.style.top, '-218px')
   })
+  it('Specifies a name attribute for the button', () => {
+    const wrapper = mount(<Select name='donald' onChange={noop} />)
+    assert.equal(wrapper.find('.Select-body').node.name, 'donald')
+  })
 })
