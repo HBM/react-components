2b4f7e0bc3fc8de8299056936f7e63ec7761a1de
Gerhard Preuss <gelipp@gmail.com>
2017-05-31 14:44:23 +0200

    prefix mdc Snackbar

--- src/js/snackbar/test/test.js
+++ src/js/snackbar/test/test.js
@@ -5,30 +5,30 @@ import React from 'react'
 import Snackbar from '../'
 import {mount} from 'enzyme'
 
-describe('Snackbar', () => {
+describe('mdc-Snackbar', () => {
   it('should be empty by default', () => {
     const wrapper = mount(<Snackbar visible />)
-    assert.equal(wrapper.find('.Snackbar').text(), '')
+    assert.equal(wrapper.find('.mdc-Snackbar').text(), '')
   })
 
   it('should render with text', () => {
     const wrapper = mount(<Snackbar visible text='all good' />)
-    assert.equal(wrapper.find('.Snackbar').text(), 'all good')
+    assert.equal(wrapper.find('.mdc-Snackbar').text(), 'all good')
   })
 
   it('should render with text and action button', () => {
     const wrapper = mount(<Snackbar visible text='all good' action='undo' />)
-    assert.equal(wrapper.find('.Snackbar-action').node.value, 'undo')
+    assert.equal(wrapper.find('.mdc-Snackbar-action').node.value, 'undo')
   })
 
   it('should trigger an event when clicking on action button', (done) => {
     const callback = () => done()
     const wrapper = mount(<Snackbar visible text='all good' action='undo' onAction={callback} />)
-    wrapper.find('.Snackbar-action').simulate('click')
+    wrapper.find('.mdc-Snackbar-action').simulate('click')
   })
 
   it('should be hidden by default', () => {
     const wrapper = mount(<Snackbar />)
-    assert.equal(wrapper.find('.Snackbar').length, 0)
+    assert.equal(wrapper.find('.mdc-Snackbar').length, 0)
   })
 })
