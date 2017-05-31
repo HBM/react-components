7c3acaa09864eb24adf1e02b66d8d3511a09ecfb
Gerhard Preuss <gelipp@gmail.com>
2017-05-31 14:22:25 +0200

    prefix mdc Card

--- src/js/card/test/test.js
+++ src/js/card/test/test.js
@@ -5,19 +5,19 @@ import React from 'react'
 import {shallow} from 'enzyme'
 import {Card, Title} from '../'
 
-describe('Card', () => {
+describe('mdc-Card', () => {
   it('should work', () => {
     const wrapper = shallow(<Card />)
-    assert.equal(wrapper.find('.Card').length, 1)
+    assert.equal(wrapper.find('.mdc-Card').length, 1)
   })
 
   it('should render the title', () => {
     const wrapper = shallow(<Title>some title</Title>)
-    assert.equal(wrapper.find('.Card-title').text(), 'some title')
+    assert.equal(wrapper.find('.mdc-Card-title').text(), 'some title')
   })
 
   it('should pass down className prop', () => {
     const wrapper = shallow(<Card className='foo' />)
-    assert.equal(wrapper.find('.Card.foo').length, 1)
+    assert.equal(wrapper.find('.mdc-Card.foo').length, 1)
   })
 })
