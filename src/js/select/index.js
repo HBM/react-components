798299fe5c672aa0eabb16e3a2d9f84599a6285e
Fahri Sönmez <fahri.soenmez@hbm.com>
2017-12-19 18:13:15 +0100

    experimental

--- src/js/select/index.js
+++ src/js/select/index.js
@@ -261,6 +261,17 @@ export class List extends React.Component {
         return event.target.previousSibling.focus()
       }
     }
+
+    const {options, selectedIndex} = this.props
+    const filterValue = event.key
+    const findIndex = options.findIndex(({label, value}, index) => {
+      let testValue = label.toLowerCase()
+      return testValue.indexOf(filterValue.toLowerCase()) >= 0
+    })
+    if (findIndex !== -1) {
+      event.preventDefault()
+      this[`li${findIndex}`].focus()
+    }
   }
 
   render () {
